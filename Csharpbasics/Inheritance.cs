12a5c9b79dceb458e343630c28d1f4932e48c85c
Sulav <Sulavshrestha631@gmail.com>
2023-06-04 23:59:08 +0545

    Learned about Inheretance

---
+++ Csharpbasics/Inheritance.cs
@@ -0,0 +1,50 @@
+using System;
+public abstract class Shape
+{
+    public abstract double GetArea();
+    public abstract double GetCircumference();
+    public virtual void PrintResult()
+    {
+        Console.WriteLine("Printing the results");
+    }
+
+}
+
+public class Rectangle : Shape
+{
+    public double Length { get; set; }
+    public double Breadth {get; set; }
+
+    public Rectangle(double length, double breadth)
+    {
+        Length = length;
+        Breadth = breadth;
+    }
+
+    public override double GetArea() => Length * Breadth;
+    public override double GetCircumference() => 2 * (Length * Breadth);
+
+    public override void PrintResult()
+    {
+        Console.WriteLine($"Printing the results of the rectangle {Length}*{Breadth}");
+        Console.WriteLine($"Area: {this.GetArea()}");
+        Console.WriteLine($"Perimeter: {this.GetCircumference()}");
+
+    }
+}
+public class Square : Rectangle
+{
+    public Square(double sides) : base(sides,sides)  // base calls parent class constructor .
+    {
+    }
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double GetArea() => Math.PI * Radius * Radius;
+
+    public override double GetCircumference() => 2 * Math.PI * Radius;
+
+}
--- Csharpbasics/Program.cs
+++ Csharpbasics/Program.cs
@@ -5,7 +5,11 @@ class Program
 {
     static void Main()
     {
-      Collections collections = new Collections();
-      collections.LearnDictionary();
+      Square square = new Square(23.45);
+      var rectangle = new Rectangle(223.5,445.6);
+      var x = square.GetArea();
+      rectangle.PrintResult();
+
+      Console.WriteLine($"Area: {x}");
     }
 }
